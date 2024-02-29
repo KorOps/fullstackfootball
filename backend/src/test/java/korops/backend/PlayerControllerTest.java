e7de5452429aa0839e2ceae73331d5a6d4b27dd0
korops <kori.braide@gmail.com>
2024-02-29 19:18:51 +0100

    add to front and backend

--- backend/src/test/java/korops/backend/PlayerControllerTest.java
+++ backend/src/test/java/korops/backend/PlayerControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
@@ -14,7 +15,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-@AutoConfiguration
+@AutoConfigureMockMvc
 class PlayerControllerTest {
 
     private final String BASE_URL = "/api/player";
@@ -31,50 +32,131 @@ class PlayerControllerTest {
     @DirtiesContext
     @Test
     void getAllPlayers_shouldReturnListOfPlayersWhenCalled() throws Exception {
-        Player player = new Player("1", "Jamie");
-        repo.save(player);
+        Player player= new Player(1, "Jamie");
+        PlayerRepo.save(player);
 
         mvc.perform(MockMvcRequestBuilders.get(BASE_URL))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(
                         """
-                                                           
-                                                   [{"firstname": "Jamie",
-                                                               "id": "1"
-                                                           }]
-                                         
+                                [
+                                 {
+                                    "id": "1",
+                                     "name": "Jamie"
+                                  
+                                 }
+                                ]
                                 """
                 ));
-        }
+    }
 
     @Test
     void getById_shouldReturnPlayerWhenCalledWithId() throws Exception {
-        NewPlayer player = NewPlayer("name");
-        String newPlayerAsJSON = objectMapper.writeValueAsString(player);
+        Player player= new Player(1, "Jamie");
+        PlayerRepo.save(player);
 
-        MvcResult result = mvc.perform(MockMvcRequestBuilders.post(BASE_URL))
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(newPlayerAsJSON)
-        .andExpect(MockMvcResultMatchers.status().isOk())
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/{id}", 1)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                                [
+                                 {
+                                    "id": "1",
+                                     "name": "Jamie"
+                                  
+                                 }
+                                ]
+                                """
+                ));
+    }
+
+    @Test
+    void savePlayer_newPlayerShouldBeSavedAndReturnedWhenCalled() throws Exception{
+        String requestBody = """
+            {
+                "name": "Jamie"
+            }
+            """;
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
 
-        Player savePlayer = objectMapper.readValue(result.getResponse().getContentAsString(), Player.class);
-        String PlayerAsJSON = objectMapper.writeValueAsString(savePlayer);
+        String savedPlayerId = result.getResponse().getContentAsString();
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/" + savedPlayerId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                        {
+                            "id":""" + savedPlayerId + """,
+                        "name": "Jamie"
+                    }
+                    """
+                ));
     }
 
     @Test
-    void savePlayer() {
+    void updatePlayer_ableToEditExistingPlayerWhenCalledAndThenReturn() throws Exception {
+            Player player = new Player(1, "Jamie");
+            Player savedPlayer = PlayerRepo.save(player);
+
+            String updatedName = "UpdatedJamie";
+            String requestBody = """
+            {
+                "name": "%s"
+            }
+            """.formatted(updatedName);
+
+            mvc.perform(MockMvcRequestBuilders.put(BASE_URL + "/" + savedPlayer.getId())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody))
+                    .andExpect(MockMvcResultMatchers.status().isOk());
+
+            mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/" + savedPlayer.getId()))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().json(
+                            """
+                            {
+                                "id": "%s",
+                                "name": "%s"
+                            }
+                            """.formatted(savedPlayer.getId(), updatedName)
+                    ));
     }
 
     @Test
-    void updatePlayer() {
+    void deletePlayer_ableToDeletePlayerWhenCalled() throws Exception {
+        Player player = new Player(1, "Jamie");
+        Player savedPlayer = PlayerRepo.save(player);
+
+        mvc.perform(MockMvcRequestBuilders.delete(BASE_URL + "/" + savedPlayer.getId()))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/" + savedPlayer.getId()))
+                .andExpect(MockMvcResultMatchers.status().isNotFound()
+                );
     }
 
-    @Test
-    void deletePlayer() {
-    }
 
     @Test
     void findPlayerByFirstnameAndLastname() {
+        Player player= new Player(1, "Jamie");
+        PlayerRepo.save(player);
+
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/find/Jamie"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """
+                                [
+                                 {
+                                    "id": "1",
+                                     "name": "Jamie"
+                                  
+                                 }
+                                ]
+                                """
+                ));
     }
 }
